5d047b5fe8e97bf5f472fdafb61ad373533373c1
HajimeNoJosh <joshuah.m.martin@gmail.com>
2019-12-06 09:19:26 -0500

    initial commit

---
+++ app/models/message.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const messageSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+    autopopulate: true
+  },
+  room: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Room',
+    required: false
+  }
+}, {
+  timestamps: true
+})
+
+messageSchema.plugin(require('mongoose-autopopulate'))
+
+module.exports = mongoose.model('Message', messageSchema)
